6439130b45675fa76c6e7bd52ac93b25eb99ad13
Faisal AL-Otaibi <faysl4041@gmail.com>
2018-10-21 16:19:11 +0300

    Update FSnapChatLoadingView.swift

--- FSnapChatLoading/FSnapChatLoadingView.swift
+++ FSnapChatLoading/FSnapChatLoadingView.swift
@@ -40,7 +40,9 @@ public class FSnapChatLoadingView: UIView {
     }
     
     public func hide() {
+        if overlayView != nil {
         overlayView.removeFromSuperview()
+        }
     }
     
     public func setColorBackground(color:UIColor){
